85170ab74528fbfebf7a1a926b46d92d6cc0f974
orson391 <newtham29@gmail.com>
2024-03-24 10:20:01 +0530

    latest

--- DataBase/pokemon.sql
+++ DataBase/pokemon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2024 at 08:00 AM
+-- Generation Time: Mar 24, 2024 at 05:49 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -78,6 +78,40 @@ CREATE TABLE `battles` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `friendships`
+--
+
+CREATE TABLE `friendships` (
+  `id` int(11) NOT NULL,
+  `user1_id` int(11) NOT NULL,
+  `user2_id` int(11) NOT NULL,
+  `friendship_start_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend_requests`
+--
+
+CREATE TABLE `friend_requests` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `request_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `friend_requests`
+--
+
+INSERT INTO `friend_requests` (`id`, `sender_id`, `recipient_id`, `request_time`) VALUES
+(1, 9, 14, '2024-03-23 13:03:51'),
+(2, 14, 9, '2024-03-23 17:50:55');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `items`
 --
 
@@ -121,6 +155,55 @@ CREATE TABLE `pokemon` (
   `base_special_defense` int(11) DEFAULT NULL,
   `base_speed` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pokemon`
+--
+
+INSERT INTO `pokemon` (`pokemon_id`, `name`, `type1`, `type2`, `base_hp`, `base_attack`, `base_defense`, `base_special_attack`, `base_special_defense`, `base_speed`) VALUES
+(1, 'Bulbasaur', 'Grass', 'Poison', NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'Ivysaur', 'Grass', 'Poison', NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'Venusaur', 'Grass', 'Poison', NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'Charmander', 'Fire', 'Fire', NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'Charmeleon', 'Fire', 'Fire', NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'Charizard', 'Fire', 'Flying', NULL, NULL, NULL, NULL, NULL, NULL),
+(7, 'Squirtle', 'Water', 'Water', NULL, NULL, NULL, NULL, NULL, NULL),
+(8, 'Wartortle', 'Water', 'Water', NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'Blastoise', 'Water', 'Water', NULL, NULL, NULL, NULL, NULL, NULL),
+(10, 'Caterpie', 'Bug', 'Bug', NULL, NULL, NULL, NULL, NULL, NULL),
+(11, 'Weedle', 'Bug', 'Poison', NULL, NULL, NULL, NULL, NULL, NULL),
+(12, 'Kakuna', 'Bug', 'Poison', NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `random_fights`
+--
+
+CREATE TABLE `random_fights` (
+  `id` int(11) NOT NULL,
+  `fight_number` int(11) DEFAULT NULL,
+  `team_number` int(11) DEFAULT NULL,
+  `pokemon_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `random_fights`
+--
+
+INSERT INTO `random_fights` (`id`, `fight_number`, `team_number`, `pokemon_id`) VALUES
+(1, 1, 1, 1),
+(2, 1, 1, 2),
+(3, 1, 1, 3),
+(4, 1, 1, 4),
+(5, 1, 1, 5),
+(6, 1, 1, 6),
+(7, 1, 2, 7),
+(8, 1, 2, 8),
+(9, 1, 2, 9),
+(10, 1, 2, 10),
+(11, 1, 2, 11),
+(12, 1, 2, 12);
 
 -- --------------------------------------------------------
 
@@ -183,7 +266,15 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password_hash`, `email`, `registration_date`) VALUES
-(1, '122', 'c6f3ac57944a531490cd39902d0f777715fd005efac9a30622d5f5205e7f6894', NULL, '2024-03-17 06:59:14');
+(2, 'orson391', 'a934c244755c66aebb0d6f9f5687038ffae8f00b00b28b4e17521016393f38b9', NULL, '2024-03-17 07:05:39'),
+(3, 'amal', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', NULL, '2024-03-17 07:09:39'),
+(4, '123', 'e7f6c011776e8db7cd330b54174fd76f7d0216b612387a5ffcfb81e6f0919683', NULL, '2024-03-17 07:16:22'),
+(5, 'notnabeel', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2024-03-17 07:30:52'),
+(6, 'notgoutham', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2024-03-17 07:35:20'),
+(9, 'arun', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2024-03-17 10:19:40'),
+(13, 'Goutham', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2024-03-17 10:48:58'),
+(14, 'niga', 'b1106bd2a8594910c2e8e1c5b0e8cc948694f0fdc5235bfbbc41f5ec3e04d25e', NULL, '2024-03-23 04:25:08'),
+(15, 'noga', 'b1106bd2a8594910c2e8e1c5b0e8cc948694f0fdc5235bfbbc41f5ec3e04d25e', NULL, '2024-03-23 12:20:20');
 
 --
 -- Indexes for dumped tables
@@ -220,6 +311,22 @@ ALTER TABLE `battles`
   ADD KEY `winner_id` (`winner_id`);
 
 --
+-- Indexes for table `friendships`
+--
+ALTER TABLE `friendships`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user1_id` (`user1_id`),
+  ADD KEY `user2_id` (`user2_id`);
+
+--
+-- Indexes for table `friend_requests`
+--
+ALTER TABLE `friend_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `recipient_id` (`recipient_id`);
+
+--
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
@@ -239,6 +346,13 @@ ALTER TABLE `moves`
 ALTER TABLE `pokemon`
   ADD PRIMARY KEY (`pokemon_id`),
   ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `random_fights`
+--
+ALTER TABLE `random_fights`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pokemon_id` (`pokemon_id`);
 
 --
 -- Indexes for table `staticeffects`
@@ -297,6 +411,18 @@ ALTER TABLE `battles`
   MODIFY `battle_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `friendships`
+--
+ALTER TABLE `friendships`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `friend_requests`
+--
+ALTER TABLE `friend_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
@@ -312,7 +438,13 @@ ALTER TABLE `moves`
 -- AUTO_INCREMENT for table `pokemon`
 --
 ALTER TABLE `pokemon`
-  MODIFY `pokemon_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pokemon_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `random_fights`
+--
+ALTER TABLE `random_fights`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `staticeffects`
@@ -336,7 +468,7 @@ ALTER TABLE `teams`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
@@ -361,6 +493,26 @@ ALTER TABLE `battles`
   ADD CONSTRAINT `battles_ibfk_1` FOREIGN KEY (`user1_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `battles_ibfk_2` FOREIGN KEY (`user2_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `battles_ibfk_3` FOREIGN KEY (`winner_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `friendships`
+--
+ALTER TABLE `friendships`
+  ADD CONSTRAINT `friendships_ibfk_1` FOREIGN KEY (`user1_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `friendships_ibfk_2` FOREIGN KEY (`user2_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `friend_requests`
+--
+ALTER TABLE `friend_requests`
+  ADD CONSTRAINT `friend_requests_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `friend_requests_ibfk_2` FOREIGN KEY (`recipient_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `random_fights`
+--
+ALTER TABLE `random_fights`
+  ADD CONSTRAINT `random_fights_ibfk_1` FOREIGN KEY (`pokemon_id`) REFERENCES `pokemon` (`pokemon_id`);
 
 --
 -- Constraints for table `teams`
